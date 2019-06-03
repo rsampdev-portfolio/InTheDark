8d41059fc1d0cf49b93ae0e2d493045322bfebb9
rsampdev <rsampdev@gmail.com>
2019-06-02 22:37:33 -0400

    GameLoop and Player classes

---
+++ In The Dark/src/com/rsampdev/inTheDark/GameLoop.java
@@ -0,0 +1,21 @@
+package com.rsampdev.inTheDark;
+
+class GameLoop {
+
+	public static void main(String[] args) {
+
+		gameLoop();
+
+	}
+
+	static void gameLoop() {
+
+		boolean running = true;
+
+		while (running) {
+			break; // <-- Game logic goes here
+		}
+
+	}
+
+}
---
+++ In The Dark/src/com/rsampdev/inTheDark/Player.java
@@ -0,0 +1,20 @@
+package com.rsampdev.inTheDark;
+
+class Player {
+
+	int health;
+	int experience;
+
+	void attack() {
+
+	}
+
+	void use() {
+
+	}
+
+	void retreat() {
+
+	}
+
+}

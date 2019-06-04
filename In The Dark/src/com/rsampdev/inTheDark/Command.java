04bde7355d1701081f0b7043a08e40182bf7f6c0
rsampdev <rsampdev@gmail.com>
2019-06-03 20:17:49 -0400

    fighting setup and game loop restructuring

--- In The Dark/src/com/rsampdev/inTheDark/Command.java
+++ In The Dark/src/com/rsampdev/inTheDark/Command.java
@@ -2,7 +2,7 @@ package com.rsampdev.inTheDark;
 
 enum Command {
 
-	HELP("help"), QUIT("quit"), EXPLORE("explore"), STATS("stats");
+	HELP("help"), QUIT("quit"), YES("yes"), NO("no"), EXPLORE("explore"), STATS("stats");
 
 	private String value;
 
--- In The Dark/src/com/rsampdev/inTheDark/Game.java
+++ In The Dark/src/com/rsampdev/inTheDark/Game.java
@@ -36,66 +36,71 @@ class Game {
 		return commandListener;
 	}
 
-	void explore(Scanner terminal) {
+	private void explore(Scanner terminal) {
 		int number = dice.nextInt(6);
 
-		String input = "";
-
 		if (number <= 1) {
-
 			System.out.println("You have encountered nothing. But the tunnel continues onward...");
-
 		}
 
 		if (number == 2) {
-
-			while (!input.equals("left") && !input.equals("right")) {
-				System.out.println("You have come to an intersection, do you go left of right?");
-				input = terminal.nextLine().toLowerCase().trim();
-			}
-
-			explore(terminal);
-
+			intersection(terminal);
 		}
 
 		if (number == 3) {
-
-			Item item = Item.getRandomItem();
-			player.addItem(item);
-
-			System.out.println("\nYou have found a(n) " + item.getName() + "\n");
-
-			while (!input.equals("yes") && !input.equals("no")) {
-				System.out.println("Do you want to use the " + item.getName() + " now?\n");
-
-				input = terminal.nextLine().toLowerCase().trim();
-
-				if (input.equals("yes")) {
-					player.useItem(item.getName());
-				}
-			}
-
+			foundItem(terminal);
 		}
 
 		if (number == 4) {
-
-			System.out.println("You have encountered an enemy.");
+			fight(terminal);
 
 			// Enemies and monsters
-
 		}
 
 		if (number == 5) {
-
 			System.out.println("Another path...");
 
 			// Another path...
-
 		}
 
 	}
 
-	void stats() {
+	private void intersection(Scanner terminal) {
+		String input = "";
+
+		while (!input.equals("left") && !input.equals("right")) {
+			System.out.println("You have come to an intersection, do you go left of right?");
+			input = terminal.nextLine().toLowerCase().trim();
+		}
+
+		explore(terminal);
+	}
+
+	private void foundItem(Scanner terminal) {
+		String input = "";
+
+		Item item = Item.getRandomItem();
+		player.addItem(item);
+
+		System.out.println("\nYou have found a(n) " + item.getName() + "\n");
+		System.out.println("ENTER: yes or no");
+
+		while (!input.equals(Command.YES.getValue()) && !input.equals(Command.NO.getValue())) {
+			System.out.println("Do you want to use the " + item.getName() + " now?\n");
+
+			input = terminal.nextLine().toLowerCase().trim();
+
+			if (input.equals("yes")) {
+				player.useItem(item.getName());
+			}
+		}
+	}
+
+	private void fight(Scanner terminal) {
+		System.out.println("You have encountered an enemy.");
+	}
+
+	private void stats() {
 		System.out.println(player.getStats() + "\n");
 	}
 

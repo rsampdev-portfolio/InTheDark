3c715515129e94cc37815a21890983c62f48f903
rsampdev <rsampdev@gmail.com>
2019-07-08 21:15:28 -0400

    work on weapons

--- In The Dark/src/com/rsampdev/inTheDark/Weapon.java
+++ In The Dark/src/com/rsampdev/inTheDark/Weapon.java
@@ -6,14 +6,38 @@ import java.util.List;
 
 enum Weapon {
 
-	NONE(0, "literally nothing"), FIST(5, "Fist"), WOODEN_SWORD(10, "Wooden Sword"), STONE_SWORD(20, "Stone Sword"), IRON_SWORD(30, "Iron Sword"), STEEL_SWORD(40, "Steel Sword"),
+	// Basic; 0-1
+
+	NONE(0, "literally nothing"), FIST(5, "Fist"),
+
+	// Swords: 2-5
+
+	WOODEN_SWORD(10, "Wooden Sword"), STONE_SWORD(20, "Stone Sword"), IRON_SWORD(30, "Iron Sword"), STEEL_SWORD(40, "Steel Sword"),
+
+	// Clubs: 6-7
+
 	WOODEN_CLUB(20, "Wooden Club"), IRON_REINFORCED_CLUB(30, "Iron Reinforced Club");
+
+	// Animals: 8 ->
+
+	// Coming soon...
 
 	private double damage;
 	private String description;
 
 	private static final List<Weapon> WEAPONS = Collections.unmodifiableList(Arrays.asList(values()));
-	private static final int SIZE = WEAPONS.size();
+	private static final List<Weapon> BASIC_WEAPONS = WEAPONS.subList(0, 2);
+	private static final List<Weapon> SWORD_WEAPONS = WEAPONS.subList(2, 6);
+	private static final List<Weapon> CLUB_WEAPONS = WEAPONS.subList(6, 8);
+
+	// private static final List<Weapon> ANIMAL_WEAPONS = WEAPONS.subList(0, 2);
+
+	private static final int WEAPONS_SIZE = WEAPONS.size();
+	private static final int BASIC_WEAPONS_SIZE = WEAPONS.size();
+	private static final int SWORD_WEAPONS_SIZE = WEAPONS.size();
+	private static final int CLUB_WEAPONS_SIZE = WEAPONS.size();
+
+	// private static final int ANIMAL_WEAPONS_SIZE = WEAPONS.size();
 
 	private Weapon(double damage, String description) {
 		this.description = description;
@@ -33,7 +57,19 @@ enum Weapon {
 	}
 
 	static Weapon getRandomWeapon() {
-		return WEAPONS.get(2 + Tools.DICE.nextInt(SIZE - 2));
+		return WEAPONS.get(Tools.DICE.nextInt(WEAPONS_SIZE- 2) + 2);
+	}
+
+	static Weapon getRandomBasicWeapon() {
+		return BASIC_WEAPONS.get(Tools.DICE.nextInt(BASIC_WEAPONS_SIZE));
+	}
+
+	static Weapon getRandomSwordWeapon() {
+		return SWORD_WEAPONS.get(Tools.DICE.nextInt(SWORD_WEAPONS_SIZE));
+	}
+
+	static Weapon getRandomClubWeapon() {
+		return CLUB_WEAPONS.get(Tools.DICE.nextInt(CLUB_WEAPONS_SIZE));
 	}
 
 	String getStats() {
